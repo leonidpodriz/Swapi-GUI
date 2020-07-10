5e74e8aed54b3bfd691ca6b29ef11e6cbc25c6a6
leonidpodriz@gmail.com <xX13y53ab0Perfect~>
2020-07-10 16:11:09 +0300

    Updates

---
+++ precache-manifest.e38408a0aa0c5a3c0af3c667040772e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1a40548cfae82bddf9c9937e492c88",
+    "url": "/Swapi-GUI/index.html"
+  },
+  {
+    "revision": "b170d75ffa94645917ca",
+    "url": "/Swapi-GUI/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b170d75ffa94645917ca",
+    "url": "/Swapi-GUI/static/js/2.72368563.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Swapi-GUI/static/js/2.72368563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25833c2de558527822cb",
+    "url": "/Swapi-GUI/static/js/main.0406265e.chunk.js"
+  },
+  {
+    "revision": "6070e814aa146262ee23",
+    "url": "/Swapi-GUI/static/js/runtime-main.e2a6af04.js"
+  },
+  {
+    "revision": "e76c8d59f36b95f77fda8e1c7e231688",
+    "url": "/Swapi-GUI/static/media/error-banner.e76c8d59.png"
+  }
+]);
