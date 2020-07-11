8edd25c40f5deca4f29ee268e32b2ded8166cf35
leonidpodriz@gmail.com <xX13y53ab0Perfect~>
2020-07-11 15:17:00 +0300

    update container update path resolve

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,25 +1,21 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import DetailsList from "../details-list";
 import {FilmsPage} from "../pages";
 import Header from "../header";
+import Container from "../container";
 
 
 const App = () => {
     return (
         <Router>
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-9 col-lg-5">
-                        <Header/>
-
-                        <Route path="/" exact>
-                            <FilmsPage/>
-                        </Route>
-                        <Route path={document.location.pathname + ":entity/:id"} component={DetailsList}/>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Header/>
+                <Route path="/" exact component={FilmsPage} />
+                <Route path={"/:entity/:id"} component={DetailsList} />
+                <Route render={() => <Redirect to="/" />}/>
+            </Container>
         </Router>
     )
 }
