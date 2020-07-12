bfbd9a8a8264f4a53277a23f8bf84a4e223e4c67
leonidpodriz@gmail.com <xX13y53ab0Perfect~>
2020-07-12 11:08:32 +0300

    Updates

---
+++ precache-manifest.8f1234869edcc79feed787eea56d0883.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6477d7a203dc8f177757cefb95c368",
+    "url": "/Swapi-GUI/index.html"
+  },
+  {
+    "revision": "62345985b2110c2d989d",
+    "url": "/Swapi-GUI/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4ff2d4e4d06fcc6f0b38",
+    "url": "/Swapi-GUI/static/css/main.137d6c0b.chunk.css"
+  },
+  {
+    "revision": "62345985b2110c2d989d",
+    "url": "/Swapi-GUI/static/js/2.aaa8d044.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Swapi-GUI/static/js/2.aaa8d044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff2d4e4d06fcc6f0b38",
+    "url": "/Swapi-GUI/static/js/main.94abd339.chunk.js"
+  },
+  {
+    "revision": "6070e814aa146262ee23",
+    "url": "/Swapi-GUI/static/js/runtime-main.e2a6af04.js"
+  },
+  {
+    "revision": "e76c8d59f36b95f77fda8e1c7e231688",
+    "url": "/Swapi-GUI/static/media/error-banner.e76c8d59.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.782dc683ea10c0f6224955144828a72d.js"
+  "/Swapi-GUI/precache-manifest.8f1234869edcc79feed787eea56d0883.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Swapi-GUI/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
