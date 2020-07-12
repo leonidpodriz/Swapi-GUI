fa78a9eec1ae87595043deac4ad70492152d58bb
leonidpodriz@gmail.com <xX13y53ab0Perfect~>
2020-07-12 10:40:42 +0300

    Updates

---
+++ precache-manifest.782dc683ea10c0f6224955144828a72d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdf7429cd576e9fffcab1b2fb506dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae9a9ecffaf54525f47",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "105585f3508965ab1a1b",
+    "url": "/static/css/main.137d6c0b.chunk.css"
+  },
+  {
+    "revision": "bae9a9ecffaf54525f47",
+    "url": "/static/js/2.f984a92d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f984a92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105585f3508965ab1a1b",
+    "url": "/static/js/main.cf9e1df5.chunk.js"
+  },
+  {
+    "revision": "c34af8602fd97b2ea0cb",
+    "url": "/static/js/runtime-main.785d824e.js"
+  },
+  {
+    "revision": "e76c8d59f36b95f77fda8e1c7e231688",
+    "url": "/static/media/error-banner.e76c8d59.png"
+  }
+]);
